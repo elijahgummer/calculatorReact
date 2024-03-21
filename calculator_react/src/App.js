32f84631f3c0293ff5342a9e70adcb944c5ebc39
Elijah Gummer <elijahgummer5@gmail.com>
2024-03-21 11:51:15 +1000

    changed the operhand thingo

--- calculator_react/src/App.js
+++ calculator_react/src/App.js
@@ -3,7 +3,10 @@
 function App() {
   return (
     <div className="calculator-grid">
-      
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
     </div>
   );
 }
